14b460fdf05cc87f276b4cb3420ed6c4fcbd6931
maulana dimyati <dimyatimaulana@gmail.com>
2023-01-21 09:02:33 +0700

    add 2nd endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 const api = (() => {
   const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
   const BASE_URL = 'https://indonesia-public-static-api.vercel.app/api/heroes';
+  const BASE_URL2 = 'https://19104008-mhs.bdv.or.id/api/heroes.php';
 
   async function getHeroes() {
     const response = await fetch(CORS_PROXY + BASE_URL, {
@@ -15,7 +16,15 @@ const api = (() => {
     return heroes;
   }
 
-  return { getHeroes };
+  async function getHeroes2() {
+    const response = await fetch(BASE_URL2);
+    const responseJson = await response.json();
+    const heroes2 = responseJson;
+
+    return heroes2;
+  }
+
+  return { getHeroes, getHeroes2 };
 })();
 
 export default api;
