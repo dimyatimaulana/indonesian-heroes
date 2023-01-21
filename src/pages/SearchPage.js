addd79e02fa06418e14a5d6a3ae8abf256c53117
maulana dimyati <dimyatimaulana@gmail.com>
2023-01-21 09:02:12 +0700

    add var filtered heroes and search params state

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,21 +1,43 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
-import SearchBar from '../components/SearchBar';
+import SearchBar from '../components/search/SearchBar';
+import BackButton from '../components/buttons/BackButton';
+import CardsList from '../components/card/CardsList';
+import '../styles/SearchPage.css';
 
 function SearchPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [keyword, setKeyword] = useState(() => searchParams.get('keywordSearch') || '');
+
+  const heros = JSON.parse(localStorage.getItem('heroes'));
+
+  const filteredHeroes = heros.filter(
+    (hero) => hero.name.toLowerCase().includes(
+      keyword.toLocaleLowerCase(),
+    ),
+  );
 
   function onKeywordChangeHandler(keywordSearch) {
     setKeyword(keywordSearch);
     setSearchParams({ keywordSearch });
   }
 
+  if (filteredHeroes.length === 0) {
+    return (
+      <div className="search-page">
+        <Link to="/"><BackButton /></Link>
+        <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler} />
+        <p className="hasil-kosong">Hasil tidak ditemukan</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="page-container">
-      <button type="button"><Link to="/">Kembali</Link></button>
+    <div className="search-page">
+      <Link to="/"><BackButton /></Link>
       <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler} />
+      <CardsList heroes={filteredHeroes} />
     </div>
   );
 }
